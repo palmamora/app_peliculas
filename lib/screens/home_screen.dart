6fed56a74dbb671cb2b6c104f64d367ca929c3c6
Manuel Eduardo Palma Mora <m.palma12@ufromail.cl>
2021-09-01 23:05:31 -0400

    se anadió card-swiper

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Películas en cines'),
-      elevation: 0.0,
-      ),
-      body: Container(
-        child: Center(
-          child: Text('home screen'),
+      appBar: AppBar(
+        title: Text(
+          'Películas en cines',
         ),
+        elevation: 0.0,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search_outlined))
+        ],
       ),
+      body: Column(children: [
+        CardSwiper()
+      ]),
     );
   }
 }
